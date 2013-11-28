993a55eaec83c65f42910ffca62255e345364966
鮎綿 獏弥 <niwaton@niwaton-no-MacBook-Air.local>
2013-11-28 23:45:26 +0900

    新しい更新

---
+++ kni.rb
@@ -0,0 +1,22 @@
+#<encoding:UTF-8>
+
+class Kni
+end
+
+genko = Kni.new
+
+input_value = gets
+input_number = input_value.to_i
+puts input_number = input_number *2
+kni = 3;
+if kni > 1
+  puts 'kni氏の玉は一つです'
+end
+
+while 1 > 0
+  print "ちゃぷすのコラ画像は" 
+  print kni 
+  puts "枚です\n"
+  sleep rand()
+  kni = kni + 1
+end
